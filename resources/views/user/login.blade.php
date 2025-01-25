bbcb3ac274aacc4a71944feac59fe6deb5f4878e
Shreyansh284 <shreyanshranpariya@gmail.com>
2025-01-25 09:16:00 +0530

    added ui for signin signup

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,124 +1,114 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('user.master')
+@section('content')
+            <!--====== Section 1 ======-->
+            <div class="u-s-p-y-60">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration Form</title>
+                <!--====== Section Content ======-->
+                <div class="section__content">
+                    <div class="container">
+                        <div class="breadcrumb">
+                            <div class="breadcrumb__wrap">
+                                <ul class="breadcrumb__list">
+                                    <li class="has-separator">
 
+                                        <a href="index.html">Home</a></li>
+                                    <li class="is-marked">
 
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ URL::to('/') }}/user/css/login-register.css">
-
-</head>
-
-<body>
-    <section class="vh-100 d-flex align-items-center justify-content-center">
-        <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh;">
-            <div class="row d-flex justify-content-center align-items-center">
-                <div class="col-lg-12">
-                    <div class="card card-custom overflow-hidden">
-                        <div class="row g-0">
-                            <div class="col-lg-6 image-container">
-                                <img src="{{ URL::to('/') }}/user/img/login-register/Fashion-girl.jpg"
-                                    class="img-fluid" alt="Fashion Girl">
-                            </div>
-                            <div class="col-lg-6 p-5">
-                                @error('email')
-                                    <div class="text-danger text-center"><h3>{{ $message }}</h3></div>
-                                @enderror
-                                <h3 class="text-center mb-4">Login</h3>
-                                <form method="post" action="{{ route('login_action') }}">
-                                    @csrf
-                                    <div class="form-outline mb-3">
-                                        <label class="form-label" for="form3Example3">Email address</label>
-                                        <input type="email" id="form3Example3" class="form-control form-control-lg"
-                                            name="email" placeholder="Enter a valid email address" />
-                                    </div>
-
-                                    <div class="form-outline mb-3">
-                                        <label class="form-label" for="form3Example4">Password</label>
-                                        <input type="password" id="form3Example4" class="form-control form-control-lg"
-                                            name="pwd" placeholder="Enter password" />
-                                    </div>
-
-                                    <div class="divider d-flex align-items-center my-3">
-                                        <p class="text-center fw-bold mx-3 mb-0">Or</p>
-                                    </div>
-
-                                    <div class="d-flex flex-row align-items-center justify-content-center mb-3">
-                                        <p class="lead fw-normal mb-0 me-3">Sign in with</p>
-
-                                        <button
-                                            class="btn bg-danger btn-floating mx-1 align-items-center  justify-content-center">
-                                            <img src="{{ URL::to('/') }}/user/img/login-register/google.svg"
-                                                alt="" width="20px">
-                                        </button>
-                                    </div>
-                                    <div class="d-flex justify-content-between align-items-center mb-3">
-                                        <a href="{{ route('forgot_password') }}" class="text-body">Forgot password?</a>
-                                    </div>
-
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-danger btn-lg w-100">Login</button>
-                                        <p class="mt-3 mb-0">Don't have an account?
-                                            <a href="{{ route('registration') }}" class="link-danger">Register</a>
-                                        </p>
-                                    </div>
-                                </form>
+                                        <a href="signin.html">Signin</a></li>
+                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-    <!-- jQuery Validation -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $("form").validate({
-                rules: {
-                    email: {
-                        required: true,
-                        email: true
-                    },
-                    pwd: {
-                        required: true,
-                    }
-                },
-                messages: {
-                    email: {
-                        required: "Please enter your email address",
-                        email: "Please enter a valid email address"
-                    },
-                    pwd: {
-                        required: "Please enter a password"
-                    }
-                },
-                errorPlacement: function(error, element) {
-                    // Custom error placement: Show the error message inside the placeholder
-                    element.attr("placeholder", error.text());
-                },
-                highlight: function(element) {
-                    // Highlight the field if it's invalid
-                    $(element).addClass("is-invalid");
-                },
-                unhighlight: function(element) {
-                    // Remove the highlight if the field becomes valid
-                    $(element).removeClass("is-invalid");
-                }
-            });
-        });
-    </script>
-
-</body>
+            <!--====== End - Section 1 ======-->
 
 
-</html>
+            <!--====== Section 2 ======-->
+            <div class="u-s-p-b-60">
+
+                <!--====== Section Intro ======-->
+                <div class="section__intro u-s-m-b-60">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="section__text-wrap">
+                                    <h1 class="section__heading u-c-secondary">ALREADY REGISTERED?</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--====== End - Section Intro ======-->
+
+
+                <!--====== Section Content ======-->
+                <div class="section__content">
+                    <div class="container">
+                        <div class="row row--center">
+                            <div class="col-lg-6 col-md-8 u-s-m-b-30">
+                                <div class="l-f-o">
+                                    <div class="l-f-o__pad-box">
+                                        <h1 class="gl-h1">I'M NEW CUSTOMER</h1>
+
+                                        <span class="gl-text u-s-m-b-30">By creating an account with our store, you will be able to move through the checkout process faster, store shipping addresses, view and track your orders in your account and more.</span>
+                                        <div class="u-s-m-b-15">
+
+                                            <a class="l-f-o__create-link btn--e-transparent-brand-b-2" href="signup.html">CREATE AN ACCOUNT</a></div>
+                                        <h1 class="gl-h1">SIGNIN</h1>
+
+                                        <span class="gl-text u-s-m-b-30">If you have an account with us, please log in.</span>
+                                        <form class="l-f-o__form">
+                                            <div class="gl-s-api">
+                                                <div class="u-s-m-b-15">
+
+                                                    <button class="gl-s-api__btn gl-s-api__btn--fb" type="button"><i class="fab fa-facebook-f"></i>
+
+                                                        <span>Signin with Facebook</span></button></div>
+                                                <div class="u-s-m-b-15">
+
+                                                    <button class="gl-s-api__btn gl-s-api__btn--gplus" type="button"><i class="fab fa-google"></i>
+
+                                                        <span>Signin with Google</span></button></div>
+                                            </div>
+                                            <div class="u-s-m-b-30">
+
+                                                <label class="gl-label" for="login-email">E-MAIL *</label>
+
+                                                <input class="input-text input-text--primary-style" type="text" id="login-email" placeholder="Enter E-mail"></div>
+                                            <div class="u-s-m-b-30">
+
+                                                <label class="gl-label" for="login-password">PASSWORD *</label>
+
+                                                <input class="input-text input-text--primary-style" type="text" id="login-password" placeholder="Enter Password"></div>
+                                            <div class="gl-inline">
+                                                <div class="u-s-m-b-30">
+
+                                                    <button class="btn btn--e-transparent-brand-b-2" type="submit">LOGIN</button></div>
+                                                <div class="u-s-m-b-30">
+
+                                                    <a class="gl-link" href="lost-password.html">Lost Your Password?</a></div>
+                                            </div>
+                                            <div class="u-s-m-b-30">
+
+                                                <!--====== Check Box ======-->
+                                                <div class="check-box">
+
+                                                    <input type="checkbox" id="remember-me">
+                                                    <div class="check-box__state check-box__state--primary">
+
+                                                        <label class="check-box__label" for="remember-me">Remember Me</label></div>
+                                                </div>
+                                                <!--====== End - Check Box ======-->
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--====== End - Section Content ======-->
+            </div>
+            <!--====== End - Section 2 ======-->
+@endsection
--- resources/views/user/registration.blade.php
+++ resources/views/user/registration.blade.php
@@ -1,149 +1,136 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('user.master')
+@section('content')
+            <!--====== Section 1 ======-->
+            <div class="u-s-p-y-60">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration Form</title>
+                <!--====== Section Content ======-->
+                <div class="section__content">
+                    <div class="container">
+                        <div class="breadcrumb">
+                            <div class="breadcrumb__wrap">
+                                <ul class="breadcrumb__list">
+                                    <li class="has-separator">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ URL::to('/') }}/user/css/login-register.css">
+                                        <a href="index.html">Home</a></li>
+                                    <li class="is-marked">
 
-</head>
-
-<body>
-    <section class="vh-100 d-flex align-items-center justify-content-center">
-        <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh;">
-            <div class="row d-flex justify-content-center align-items-center">
-                <div class="col-lg-12">
-                    <div class="card card-custom overflow-hidden">
-                        <div class="row g-0">
-                            <div class="col-lg-6 image-container">
-                                <img src="{{ URL::to('/') }}/user/img/login-register/Fashion-girl.jpg"
-                                    class="img-fluid" alt="Fashion Girl">
-                            </div>
-                            <div class="col-lg-6 p-4">
-                                <h3 class="text-center">Registration</h3>
-
-                                <form method="post" action="{{ route('registration_action') }}">
-                                    @csrf
-                                    <div class="form-outline mb-1">
-                                        <label class="form-label" for="form3Example1">Full Name</label>
-                                        <input type="text" id="form3Example1" name="name" class="form-control"
-                                            placeholder="Enter your full name" required/>
-                                    </div>
-
-                                    <div class="form-outline mb-1">
-                                        <label class="form-label" for="form3Example2">Phone number</label>
-                                        <input type="text" id="form3Example2" name="number" class="form-control"
-                                            placeholder="Enter your Phone number" minlength="10" maxlength="10" required/>
-                                    </div>
-
-                                    <div class="form-outline mb-1">
-                                        <label class="form-label" for="form3Example3">Email address</label>
-                                        <input type="email" id="form3Example3" name="email" class="form-control"
-                                            placeholder="Enter a valid email address" required />
-                                    </div>
-
-                                    <div class="form-outline mb-1">
-                                        <label class="form-label" for="form3Example4">Password</label>
-                                        <input type="password" id="form3Example4" name="pwd" class="form-control"
-                                            placeholder="Enter password" required/>
-                                    </div>
-
-                                    <div class="divider d-flex align-items-center my-2">
-                                        <p class="text-center fw-bold mx-3 mb-0">Or</p>
-                                    </div>
-
-                                    <div class="d-flex flex-row align-items-center justify-content-center mb-3">
-                                        <p class="lead fw-normal mb-0 me-3">Sign up with</p>
-
-                                        <button
-                                            class="btn bg-danger btn-floating mx-1 align-items-center  justify-content-center">
-                                            <img src="{{ URL::to('/') }}/user/img/login-register/google.svg"
-                                                alt="" width="20px">
-                                        </button>
-                                    </div>
-
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-danger btn-lg w-100">Register</button>
-                                        <p class="mt-3 mb-0">Already have an account?
-                                            <a href="{{ route('login') }}" class="link-danger">Login</a>
-                                        </p>
-                                    </div>
-                                </form>
-
+                                        <a href="signup.html">Signup</a></li>
+                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            <!--====== End - Section 1 ======-->
 
-    <!-- jQuery Validation -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $("form").validate({
-                rules: {
-                    name: {
-                        required: true,
-                    },
-                    number: {
-                        required: true,
-                        digits: true, // Only digits allowed
-                        minlength: 10, // Adjust the length as required
-                        maxlength: 10 // Adjust the length as required
-                    },
-                    email: {
-                        required: true,
-                        email: true
-                    },
-                    pwd: {
-                        required: true,
-                        minlength: 8
-                    }
-                },
-                messages: {
-                    name: {
-                        required: "Please enter your full name",
-                        minlength: "Your name must be at least 3 characters long"
-                    },
-                    number: {
-                        required: "Please enter your phone number",
-                        digits: "Please enter only digits",
-                        minlength: "Phone number must be at least 10 digits",
-                        maxlength: "Phone number cannot exceed 15 digits"
-                    },
-                    email: {
-                        required: "Please enter your email address",
-                        email: "Please enter a valid email address"
-                    },
-                    pwd: {
-                        required: "Please enter a password",
-                        minlength: "Password must be at least 8 characters"
-                    }
-                },
-                errorPlacement: function(error, element) {
-                    // Custom error placement: Show the error message inside the placeholder
-                    element.attr("placeholder", error.text());
-                },
-                highlight: function(element) {
-                    // Highlight the field if it's invalid
-                    $(element).addClass("is-invalid");
-                },
-                unhighlight: function(element) {
-                    // Remove the highlight if the field becomes valid
-                    $(element).removeClass("is-invalid");
-                }
-            });
-        });
-    </script>
 
-</body>
+            <!--====== Section 2 ======-->
+            <div class="u-s-p-b-60">
 
-</html>
+                <!--====== Section Intro ======-->
+                <div class="section__intro u-s-m-b-60">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="section__text-wrap">
+                                    <h1 class="section__heading u-c-secondary">CREATE AN ACCOUNT</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--====== End - Section Intro ======-->
+
+
+                <!--====== Section Content ======-->
+                <div class="section__content">
+                    <div class="container">
+                        <div class="row row--center">
+                            <div class="col-lg-6 col-md-8 u-s-m-b-30">
+                                <div class="l-f-o">
+                                    <div class="l-f-o__pad-box">
+                                        <h1 class="gl-h1">PERSONAL INFORMATION</h1>
+                                        <form class="l-f-o__form">
+                                            <div class="gl-s-api">
+                                                <div class="u-s-m-b-15">
+
+                                                    <button class="gl-s-api__btn gl-s-api__btn--fb" type="button"><i class="fab fa-facebook-f"></i>
+
+                                                        <span>Signup with Facebook</span></button></div>
+                                                <div class="u-s-m-b-30">
+
+                                                    <button class="gl-s-api__btn gl-s-api__btn--gplus" type="button"><i class="fab fa-google"></i>
+
+                                                        <span>Signup with Google</span></button></div>
+                                            </div>
+                                            <div class="u-s-m-b-30">
+
+                                                <label class="gl-label" for="reg-fname">FIRST NAME *</label>
+
+                                                <input class="input-text input-text--primary-style" type="text" id="reg-fname" placeholder="First Name"></div>
+                                            <div class="u-s-m-b-30">
+
+                                                <label class="gl-label" for="reg-lname">LAST NAME *</label>
+
+                                                <input class="input-text input-text--primary-style" type="text" id="reg-lname" placeholder="Last Name"></div>
+                                            <div class="gl-inline">
+                                                <div class="u-s-m-b-30">
+
+                                                    <!--====== Date of Birth Select-Box ======-->
+
+                                                    <span class="gl-label">BIRTHDAY</span>
+                                                    <div class="gl-dob"><select class="select-box select-box--primary-style">
+                                                            <option selected>Month</option>
+                                                            <option value="male">January</option>
+                                                            <option value="male">February</option>
+                                                            <option value="male">March</option>
+                                                            <option value="male">April</option>
+                                                        </select><select class="select-box select-box--primary-style">
+                                                            <option selected>Day</option>
+                                                            <option value="01">01</option>
+                                                            <option value="02">02</option>
+                                                            <option value="03">03</option>
+                                                            <option value="04">04</option>
+                                                        </select><select class="select-box select-box--primary-style">
+                                                            <option selected>Year</option>
+                                                            <option value="1991">1991</option>
+                                                            <option value="1992">1992</option>
+                                                            <option value="1993">1993</option>
+                                                            <option value="1994">1994</option>
+                                                        </select></div>
+                                                    <!--====== End - Date of Birth Select-Box ======-->
+                                                </div>
+                                                <div class="u-s-m-b-30">
+
+                                                    <label class="gl-label" for="gender">GENDER</label><select class="select-box select-box--primary-style u-w-100" id="gender">
+                                                        <option selected>Select</option>
+                                                        <option value="male">Male</option>
+                                                        <option value="male">Female</option>
+                                                    </select></div>
+                                            </div>
+                                            <div class="u-s-m-b-30">
+
+                                                <label class="gl-label" for="reg-email">E-MAIL *</label>
+
+                                                <input class="input-text input-text--primary-style" type="text" id="reg-email" placeholder="Enter E-mail"></div>
+                                            <div class="u-s-m-b-30">
+
+                                                <label class="gl-label" for="reg-password">PASSWORD *</label>
+
+                                                <input class="input-text input-text--primary-style" type="text" id="reg-password" placeholder="Enter Password"></div>
+                                            <div class="u-s-m-b-15">
+
+                                                <button class="btn btn--e-transparent-brand-b-2" type="submit">CREATE</button></div>
+
+                                            <a class="gl-link" href="#">Return to Store</a>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--====== End - Section Content ======-->
+            </div>
+            <!--====== End - Section 2 ======-->
+@endsection
