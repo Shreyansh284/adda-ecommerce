9c1e19b92089d38a3bafe6f5f306204560516f10
Harpalsinh Sindhav <88263558+harpalll@users.noreply.github.com>
2025-01-30 19:15:37 +0530

    Product bug fixed.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('googleID')->nullable();
             $table->string('name');
             $table->string('email')->unique();
-            $table->bigInteger('mobile')->unique();
+            $table->bigInteger('mobile')->nullable();
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
             $table->enum('role', ['admin', 'user'])->default('user');
--- resources/views/admin/product/products.blade.php
+++ resources/views/admin/product/products.blade.php
@@ -14,68 +14,72 @@
         <div class="row">
             <div class="col-lg-12">
 
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Product</h5>
-                        <div class="d-flex justify-content-end">
-                            <a class="btn btn-primary " href="{{ URL::to('/') }}/admin/product/add">+ Add Product</a>
-                        </div>
-                        <!-- Table with stripped rows -->
-                        <table class="table datatable">
-                            <thead>
-                                <tr>
-                                    <th>Product ID</th>
-                                    <th>
-                                        <b>Product N</b>ame
-                                    </th>
-                                    <th>Price</th>
-                                    {{-- <th>Image</th> --}}
-                                    <th>Status</th>
-                                    <th>Actions</th>
-
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($products as $idx => $product)
+                @if (count($products) == 0)
+                    <h5 class="card-title">No Products</h5>
+                @else
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Product</h5>
+                            <div class="d-flex justify-content-end">
+                                <a class="btn btn-primary " href="{{ URL::to('/') }}/admin/product/add">+ Add Product</a>
+                            </div>
+                            <!-- Table with stripped rows -->
+                            <table class="table datatable">
+                                <thead>
                                     <tr>
-                                        <td>{{ $product->id }}</td>
-                                        <td>{{ $product->productName }}</td>
-                                        <td>{{ $product->price }}</td>
-                                        <td>
-                                            @if ($product->status == 'active')
-                                                <a href="{{ URL::to('/') }}/admin/product/status/{{ $product->id }}"><span
-                                                        class="badge text-bg-success">Active</span></a>
-                                            @elseif($product->status == 'inactive')
-                                                <a href="{{ URL::to('/') }}/admin/product/status/{{ $product->id }}"><span
-                                                        class="badge text-bg-warning">Inactive</span></a>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            <div>
-                                                <i class="ri-eye-fill text-primary" data-bs-toggle="modal"
-                                                    data-bs-target="#productDetailModal"></i>
+                                        <th>Product ID</th>
+                                        <th>
+                                            <b>Product N</b>ame
+                                        </th>
+                                        <th>Price</th>
+                                        {{-- <th>Image</th> --}}
+                                        <th>Status</th>
+                                        <th>Actions</th>
 
-                                                {{-- <i class="ri-edit-box-fill text-warning"></i>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($products as $idx => $product)
+                                        <tr>
+                                            <td>{{ $product->id }}</td>
+                                            <td>{{ $product->productName }}</td>
+                                            <td>{{ $product->price }}</td>
+                                            <td>
+                                                @if ($product->status == 'active')
+                                                    <a href="{{ URL::to('/') }}/admin/product/status/{{ $product->id }}"><span
+                                                            class="badge text-bg-success">Active</span></a>
+                                                @elseif($product->status == 'inactive')
+                                                    <a href="{{ URL::to('/') }}/admin/product/status/{{ $product->id }}"><span
+                                                            class="badge text-bg-warning">Inactive</span></a>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <div>
+                                                    <i class="ri-eye-fill text-primary" data-bs-toggle="modal"
+                                                        data-bs-target="#productDetailModal"></i>
+
+                                                    {{-- <i class="ri-edit-box-fill text-warning"></i>
                                                 <i class="ri-delete-bin-6-fill text-danger" data-bs-toggle="modal"
                                                     data-bs-target="#deleteProductConfirm"></i> --}}
-                                                <a href="{{ URL::to('/') }}/admin/product/edit/{{ $product->id }}">
-                                                    <i class="ri-edit-box-fill text-warning"></i>
-                                                </a>
-                                                <a href="{{ URL::to('/') }}/admin/product/delete/{{ $product->id }}">
-                                                    <i class="ri-delete-bin-6-fill text-danger"></i>
-                                                </a>
+                                                    <a href="{{ URL::to('/') }}/admin/product/edit/{{ $product->id }}">
+                                                        <i class="ri-edit-box-fill text-warning"></i>
+                                                    </a>
+                                                    <a
+                                                        href="{{ URL::to('/') }}/admin/product/delete/{{ $product->id }}">
+                                                        <i class="ri-delete-bin-6-fill text-danger"></i>
+                                                    </a>
 
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
 
-                            </tbody>
-                        </table>
-                        <!-- End Table with stripped rows -->
-
+                                </tbody>
+                            </table>
+                            <!-- End Table with stripped rows -->
+                        </div>
                     </div>
-                </div>
+                @endif
 
             </div>
         </div>
