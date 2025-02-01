097891cf7c3018b1b91e5c8dfbca44b31001d60b
Shreyansh284 <shreyanshranpariya@gmail.com>
2025-02-01 09:01:30 +0530

    PWA added

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -9,6 +9,7 @@
     <meta name="author" content="">
     <link href="{{ URL::to('/') }}/logo.png" rel="shortcut icon">
     <title>@yield('title')</title>
+    <meta name="theme-color" content="#000000"/>
 
     <!--====== Google Font ======-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet">
@@ -24,6 +25,9 @@
 
     <!--====== App ======-->
     <link rel="stylesheet" href="/user/css/app.color9.css">
+    <!-- PWA  -->
+<link rel="apple-touch-icon" href="{{URL::to('/')}}/logo.png">
+<link rel="manifest" href="{{ asset('/manifest.json') }}">
     @yield('css')
 </head>
 
@@ -40,167 +44,6 @@
 
         <!--====== Main Header ======-->
         <header class="header--style-1">
-
-            <!--====== Nav 1 ======-->
-            {{-- <nav class="primary-nav primary-nav-wrapper--border">
-                <div class="container">
-
-                    <!--====== Primary Nav ======-->
-                    <div class="primary-nav">
-
-                        <!--====== Main Logo ======-->
-
-                        <a class="main-logo" href="index.html">
-
-                            <img src="/user/images/logo/logo-1.png" alt=""></a>
-                        <!--====== End - Main Logo ======-->
-
-
-                        <!--====== Search Form ======-->
-                        <form class="main-form">
-
-                            <label for="main-search"></label>
-
-                            <input class="input-text input-text--border-radius input-text--style-1" type="text"
-                                id="main-search" placeholder="Search">
-
-                            <button class="btn btn--icon fas fa-search main-search-button" type="submit"></button>
-                        </form>
-                        <!--====== End - Search Form ======-->
-
-
-                        <!--====== Dropdown Main plugin ======-->
-                        <div class="menu-init" id="navigation">
-
-                            <button class="btn btn--icon toggle-button toggle-button--secondary fas fa-cogs"
-                                type="button"></button>
-
-                            <!--====== Menu ======-->
-                            <div class="ah-lg-mode">
-
-                                <span class="ah-close">✕ Close</span>
-
-                                <!--====== List ======-->
-                                <ul class="ah-list ah-list--design1 ah-list--link-color-secondary">
-                                    <li class="has-dropdown" data-tooltip="tooltip" data-placement="left"
-                                        title="Account">
-
-                                        <a><i class="far fa-user-circle"></i></a>
-
-                                        <!--====== Dropdown ======-->
-
-                                        <span class="js-menu-toggle"></span>
-                                        <ul style="width:120px">
-                                            <li>
-
-                                                <a href="dashboard.html"><i class="fas fa-user-circle u-s-m-r-6"></i>
-
-                                                    <span>Account</span></a>
-                                            </li>
-                                            <li>
-
-                                                <a href="signup.html"><i class="fas fa-user-plus u-s-m-r-6"></i>
-
-                                                    <span>Signup</span></a>
-                                            </li>
-                                            <li>
-
-                                                <a href="signin.html"><i class="fas fa-lock u-s-m-r-6"></i>
-
-                                                    <span>Signin</span></a>
-                                            </li>
-                                            <li>
-
-                                                <a href="signup.html"><i class="fas fa-lock-open u-s-m-r-6"></i>
-
-                                                    <span>Signout</span></a>
-                                            </li>
-                                        </ul>
-                                        <!--====== End - Dropdown ======-->
-                                    </li>
-                                    <li class="has-dropdown" data-tooltip="tooltip" data-placement="left"
-                                        title="Settings">
-
-                                        <a><i class="fas fa-user-cog"></i></a>
-
-                                        <!--====== Dropdown ======-->
-
-                                        <span class="js-menu-toggle"></span>
-                                        <ul style="width:120px">
-                                            <li class="has-dropdown has-dropdown--ul-right-100">
-
-                                                <a>Language<i class="fas fa-angle-down u-s-m-l-6"></i></a>
-
-                                                <!--====== Dropdown ======-->
-
-                                                <span class="js-menu-toggle"></span>
-                                                <ul style="width:120px">
-                                                    <li>
-
-                                                        <a class="u-c-brand">ENGLISH</a>
-                                                    </li>
-                                                    <li>
-
-                                                        <a>ARABIC</a>
-                                                    </li>
-                                                    <li>
-
-                                                        <a>FRANCAIS</a>
-                                                    </li>
-                                                    <li>
-
-                                                        <a>ESPANOL</a>
-                                                    </li>
-                                                </ul>
-                                                <!--====== End - Dropdown ======-->
-                                            </li>
-                                            <li class="has-dropdown has-dropdown--ul-right-100">
-
-                                                <a>Currency<i class="fas fa-angle-down u-s-m-l-6"></i></a>
-
-                                                <!--====== Dropdown ======-->
-
-                                                <span class="js-menu-toggle"></span>
-                                                <ul style="width:225px">
-                                                    <li>
-
-                                                        <a class="u-c-brand">$ - US DOLLAR</a>
-                                                    </li>
-                                                    <li>
-
-                                                        <a>£ - BRITISH POUND STERLING</a>
-                                                    </li>
-                                                    <li>
-
-                                                        <a>€ - EURO</a>
-                                                    </li>
-                                                </ul>
-                                                <!--====== End - Dropdown ======-->
-                                            </li>
-                                        </ul>
-                                        <!--====== End - Dropdown ======-->
-                                    </li>
-                                    <li data-tooltip="tooltip" data-placement="left" title="Contact">
-
-                                        <a href="tel:+0900901904"><i class="fas fa-phone-volume"></i></a>
-                                    </li>
-                                    <li data-tooltip="tooltip" data-placement="left" title="Mail">
-
-                                        <a href="mailto:contact@domain.com"><i class="far fa-envelope"></i></a>
-                                    </li>
-                                </ul>
-                                <!--====== End - List ======-->
-                            </div>
-                            <!--====== End - Menu ======-->
-                        </div>
-                        <!--====== End - Dropdown Main plugin ======-->
-                    </div>
-                    <!--====== End - Primary Nav ======-->
-                </div>
-            </nav> --}}
-            <!--====== End - Nav 1 ======-->
-
-
             <!--====== Nav 2 ======-->
             <nav class="secondary-nav-wrapper">
                 <div class="container">
@@ -851,7 +694,23 @@
             <!--====== End - Modal Section ======-->
         </div>
         <!--====== End - Main App ======-->
-
+        <script src="{{ asset('/sw.js') }}"></script>
+        <script>
+           if ("serviceWorker" in navigator) {
+              // Register a service worker hosted at the root of the
+              // site using the default scope.
+              navigator.serviceWorker.register("/sw.js").then(
+              (registration) => {
+                 console.log("Service worker registration succeeded:", registration);
+              },
+              (error) => {
+                 console.error(`Service worker registration failed: ${error}`);
+              },
+            );
+          } else {
+             console.error("Service workers are not supported.");
+          }
+        </script>
 
         <!--====== Google Analytics: change UA-XXXXX-Y to be your site's ID ======-->
         <script>
